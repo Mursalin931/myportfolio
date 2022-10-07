c93d1e43befc88b613aede0dcef233050021cfb6
Mursalin Hasan <83635847+Mursalin931@users.noreply.github.com>
2022-10-07 15:04:06 +0600

    Add files via upload

---
+++ precache-manifest.9a36f17913ff815ed9fd7ba3e87ed1bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e8871fe52b17b7ad760",
+    "url": "/static/css/main.678aecf5.chunk.css"
+  },
+  {
+    "revision": "7e8871fe52b17b7ad760",
+    "url": "/static/js/main.7e8871fe.chunk.js"
+  },
+  {
+    "revision": "97d0932379c375960f6c",
+    "url": "/static/js/1.97d09323.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b8ad3545ebb7ab7b662a9ec38bb60c3",
+    "url": "/index.html"
+  }
+];
